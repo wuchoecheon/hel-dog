cf695343110b0236de3415ce5d31a00ad76faa6c
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2025-11-24 01:05:21 +0900

    fhir: include stress, posture

--- src/test/test_stress.py
+++ src/test/test_stress.py
@@ -4,11 +4,11 @@ from src.main import app
 
 client = TestClient(app)
 
-def get_token():
+def get_token(email: str):
     _ = client.post(
         "/api/auth/signup",
         json={
-            "email": "user@example.com",
+            "email": email,
             "password": "asdf1234",
             "password2": "asdf1234",
         }
@@ -17,7 +17,7 @@ def get_token():
     login = client.post(
         "/api/auth/login",
         json={
-            "email": "user@example.com",
+            "email": email,
             "password": "asdf1234",
         }
     )
@@ -27,35 +27,39 @@ def get_token():
 
     return token
 
-def setup():
-    token = get_token()
+def setup(device_id: str, email: str):
+    token = get_token(email)
     response = client.post(
         "/api/device/register",
         headers={
             "Authorization": token
         },
         json={
-            "device_id": "device"
+            "device_id": device_id
         }
     )
 
+    return token
+
 def test_stress_log():
-    setup()
+    device_id = "device_stress1"
+    email = "user1@example.com"
+    setup(device_id, email)
 
     response = client.post(
-        "/api/stress/device",
+        f"/api/stress/{device_id}",
     )
 
     assert response.status_code == 200
 
 def test_get_stress_status_false():
-    setup()
+    device_id = "device_stress2"
+    email = "user2@example.com"
+    token = setup(device_id, email)
 
     client.post(
-        "/api/stress/device",
+        f"/api/stress/{device_id}",
     )
-
-    token = get_token()
 
     response = client.get(
         "/api/stress",
@@ -66,16 +70,22 @@ def test_get_stress_status_false():
 
     assert response.status_code == 200
     assert body["stressed"] is False
-    assert body["count_last_12h"] == 2
+    assert body["count_last_12h"] == 1
 
 def test_get_stress_status_true():
-    setup()
+    device_id = "device_stress3"
+    email = "user3@example.com"
+    token = setup(device_id, email)
 
     client.post(
-        "/api/stress/device",
+        f"/api/stress/{device_id}",
     )
-
-    token = get_token()
+    client.post(
+        f"/api/stress/{device_id}",
+    )
+    client.post(
+        f"/api/stress/{device_id}",
+    )
 
     response = client.get(
         "/api/stress",
